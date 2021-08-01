042dd1fa5205f0eaade55894f039b135513fce5f
Tyler <tyler.rooker@dxc.com>
2021-08-01 20:27:57 +0100

    add js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function navOneHighlight() {
+    element=document.getElementById("nav-item-1");
+    element.style.backgroundColor = "red";
+    element.addEventListener("mouseout", function() {
+    element.style.backgroundColor = "black";
+    })
+}
+function navTwoHighlight() {
+    element=document.getElementById("nav-item-2");
+    element.style.backgroundColor = "red";
+    element.addEventListener("mouseout", function() {
+    element.style.backgroundColor = "black";
+    })
+}
+function navThreeHighlight() {
+    element=document.getElementById("nav-item-3");
+    element.style.backgroundColor = "red";
+    element.addEventListener("mouseout", function() {
+    element.style.backgroundColor = "black";
+    })
+}
+function navFourHighlight() {
+    element=document.getElementById("nav-item-4");
+    element.style.backgroundColor = "red";
+    element.addEventListener("mouseout", function() {
+    element.style.backgroundColor = "black";
+    })
+}
