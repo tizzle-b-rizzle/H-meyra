512d0bd4f5947a0f2f91980a7f7cca750f719ce8
Tyler Rooker <tyler.rooker@dxc.com>
2021-08-02 12:52:52 +0100

    colourz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,32 @@
 function navOneHighlight() {
   element = document.getElementById("nav-item-1");
-  element.style.backgroundColor = "red";
+  element.style.animation = "backgroundChange 2s infinite";
   element.addEventListener("mouseout", function () {
+    element.style.animation = "none";
     element.style.backgroundColor = "#d8c3a5";
   });
 }
 function navTwoHighlight() {
   element = document.getElementById("nav-item-2");
-  element.style.backgroundColor = "red";
+  element.style.animation = "backgroundChange 2s infinite";
   element.addEventListener("mouseout", function () {
+    element.style.animation = "none";
     element.style.backgroundColor = "#d8c3a5";
   });
 }
 function navThreeHighlight() {
   element = document.getElementById("nav-item-3");
-  element.style.backgroundColor = "red";
+  element.style.animation = "backgroundChange 2s infinite";
   element.addEventListener("mouseout", function () {
+    element.style.animation = "none";
     element.style.backgroundColor = "#d8c3a5";
   });
 }
 function navFourHighlight() {
   element = document.getElementById("nav-item-4");
-  element.style.backgroundColor = "red";
+  element.style.animation = "backgroundChange 2s infinite";
   element.addEventListener("mouseout", function () {
+    element.style.animation = "none";
     element.style.backgroundColor = "#d8c3a5";
   });
 }
